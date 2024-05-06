717407142be93f9b7e0dab556d10b38a6421c4e5
Abdullah Anli <anliabdullah08@gmail.com>
2024-05-06 12:55:45 +0300

    LabReport entity created

---
+++ src/main/java/com/laboratoryreportingapplication/entity/LabReport.java
@@ -0,0 +1,71 @@
+package com.laboratoryreportingapplication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class LabReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileNumber;
+    private String diagnosisTitle;
+    private String diagnosisDetails;
+    private Date dateGiven;
+    private String photoPath;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileNumber() {
+        return fileNumber;
+    }
+
+    public void setFileNumber(String fileNumber) {
+        this.fileNumber = fileNumber;
+    }
+
+    public String getDiagnosisTitle() {
+        return diagnosisTitle;
+    }
+
+    public void setDiagnosisTitle(String diagnosisTitle) {
+        this.diagnosisTitle = diagnosisTitle;
+    }
+
+    public String getDiagnosisDetails() {
+        return diagnosisDetails;
+    }
+
+    public void setDiagnosisDetails(String diagnosisDetails) {
+        this.diagnosisDetails = diagnosisDetails;
+    }
+
+    public Date getDateGiven() {
+        return dateGiven;
+    }
+
+    public void setDateGiven(Date dateGiven) {
+        this.dateGiven = dateGiven;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+
+}
