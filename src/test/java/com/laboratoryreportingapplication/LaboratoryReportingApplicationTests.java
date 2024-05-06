26051a77560c3eee7b019a6cf5c1cb44d2ed5aba
Abdullah Anli <anliabdullah08@gmail.com>
2024-05-06 12:40:45 +0300

    project dependencies created

---
+++ src/test/java/com/laboratoryreportingapplication/LaboratoryReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratoryreportingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
